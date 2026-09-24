2c0da0a9e4de3cd8670c977e231e6cf18690ccb5
riderdevchan <34148113+riderdevchan@users.noreply.github.com>
2026-09-24 21:17:18 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,11 @@
-const CACHE_NAME = 'moto-tracker-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-512.svg'
-];
-
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
